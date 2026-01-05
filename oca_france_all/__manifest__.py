63476c1412be57ebc4ebb6040d635a1b809dec2d
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-01-05 17:35:15 +0100

    [ADD] modules proposed by alexis. (filtered for Association

--- oca_france_all/__manifest__.py
+++ oca_france_all/__manifest__.py
@@ -9,21 +9,39 @@
     "author": "Pierre Verkest, OCA France",
     "website": "https://www.oca-france.fr",
     "depends": [
-        # OCA France
-        "oca_france_base",
-        "oca_france_membership",
-        "oca_france_website_membership",
-        "oca_france_website_partner",
+        # Odoo
+        "account",
+        "contacts",
+        "membership",
+        "website",
+        # OCA/account-financial-reporting
+        "account_financial_report",
         # OCA/account-financial-tools
+        "account_dashboard_banner",
         "account_lock_date_update",
         "account_move_name_sequence",
         "account_usability",
+        # OCA/account-payment
+        "account_payment_method_base",
+        # OCA/account-reconcile
+        "account_reconcile_oca",
+        # OCA/bank-statement-import
+        "account_statement_import_file",
+        "account_statement_import_file_reconcile_oca",
+        # OCA/community-data-files
+        "account_tax_unece",
+        # OCA/edi
+        "account_invoice_facturx",
         # OCA/geospatial
         "web_leaflet_lib",
         "web_view_leaflet_map",
         "web_view_leaflet_map_partner",
+        # OCA/l10n-france
+        "l10n_fr_mis_reports",
         # OCA/mail
         "mail_debrand",
+        # OCA/mis-builder
+        "mis_builder",
         # OCA/partner-contact
         "partner_disable_gravatar",
         "partner_firstname",
@@ -57,10 +75,16 @@
         "website_odoo_debranding",
         "website_partner_form",
         "website_search_header",
-        # Odoo
-        "contacts",
-        "membership",
-        "website",
+        # oca-france/oca-france-custom
+        "oca_france_base",
+        "oca_france_membership",
+        "oca_france_website_membership",
+        "oca_france_website_partner",
+        # akretion/bank-statement-import-api
+        "account_statement_import_api",
+        "account_statement_import_api_qonto",
+        "account_statement_import_in_invoice_api",
+        "account_statement_import_in_invoice",
     ],
     "maintainers": ["petrus-v", "legalsylvain"],
     "data": [],
---
+++ oca_france_all/readme/INSTALL.md
@@ -0,0 +1,4 @@
+Uninstall the odoo module `account_edi_ubl_cii` to make the module `account_invoice_facturx`
+working correctly. See:
+
+https://github.com/OCA/edi/tree/18.0/account_invoice_facturx#installation
